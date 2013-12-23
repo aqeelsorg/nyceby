0c40d77b7c6b694bee9cab80d50081a709c5a069
aqeel <akeel@montekservices.com>
2013-12-23 14:53:11 +0530

    database.php

--- fuel/application/config/database.php
+++ fuel/application/config/database.php
@@ -43,8 +43,8 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
-$db['default']['database'] = 'fuelCMS';
+$db['default']['password'] = 'password';
+$db['default']['database'] = 'fuelcms';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
